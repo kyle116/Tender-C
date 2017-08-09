1b09ddb5ebb4f77d5385d619486ed4bd68b887ea
Kyle Liu <kyle116@yahoo.com>
2017-08-08 18:27:21 -0700

    none

--- src/components/Content.js
+++ src/components/Content.js
@@ -53,7 +53,7 @@ class Content extends React.Component{
       <div>
         <NavLink to="/matches">See your Matches</NavLink>
         <div className="image">
-          <img className="food-pic" src={this.state.images[0]} alt={this.state.businessData.name}/>
+          <img className="food-pic" src={this.state.images[0]}/>
         </div>
         <button onClick={this.unmatchButton.bind(this)}>No</button>
         <button onClick={this.matchButton.bind(this)}>Match</button>
